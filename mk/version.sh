1f55051f365616ed5d7a4c669221511c538f6887
Mirek Kratochvil <exa.exa@gmail.com>
2019-09-30 17:12:37 +0200

    New upstream version 1.9.1

--- mk/version.sh
+++ mk/version.sh
@@ -7,9 +7,9 @@ export VPREFIX="$1"
 
 # These values are filled automatically for git-archive(1) tarballs.
 # See: "export-subst" in gitattributes(5).
-distinfo_commit_full='966ba93ec1b38da4e7ee97b7207d55bcbd01e84e'
-distinfo_commit_short='966ba93e'
-distinfo_author_date='2019-09-29 17:37:07 +0200'
+distinfo_commit_full='cae7c45ea5a563c022001a54d1eee71c268f62b4'
+distinfo_commit_short='cae7c45e'
+distinfo_author_date='2019-09-29 16:51:55 +0100'
 if expr "$distinfo_commit_short" : '[0-9a-f]\{7,40\}$' >/dev/null; then
     echo "${VPREFIX}-g${distinfo_commit_short}-dist"
     exit 0
